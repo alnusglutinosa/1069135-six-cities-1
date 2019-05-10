f2ec80c39b07506bc11afe47b88c4ee4e228ce66
Ольга Шевченко <olga.app2@gmail.com>
2019-05-10 23:13:31 +0300

    валидируй, как никогда не валидировал

--- src/index.js
+++ src/index.js
@@ -2,4 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
-ReactDOM.render(<App />, document.querySelector(`#root`));
+const init = () => {
+  const settings = {
+    topPlaces: [
+      `Beautiful & luxurious apartment at great location`,
+      `Wood and stone place`,
+      `Canal View Prinsengracht`,
+      `Nice, cozy, warm big bed apartment`,
+      `Wood and stone place`
+    ]
+  };
+
+  ReactDOM.render(<App topPlaces={settings.topPlaces} />, document.querySelector(`#root`));
+};
+
+init();
